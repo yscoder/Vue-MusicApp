b5c62d4175c8eb379ad196c564241edd02618b1e
justgo <634206017@qq.com>
2016-02-16 00:37:25 +0800

    增加分页搜索

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var cheerio = require('cheerio'),
+    querystring = require('querystring'),
     request = require('../lib/request');
 
 module.exports = function(app) {
@@ -8,27 +9,39 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/search/:key', function(req, res) {
+    app.get('/favicon.ico', function(req, res) {
+        res.status(200);
+    });
 
-        // http://sou.kuwo.cn/ws/NSearch?type=all&key=曾经的你
+    app.get('/search/:key/:pn', function(req, res) {
 
-        var option = {
-            host: 'sou.kuwo.cn',
-            port: 80,
-            path: '/ws/NSearch?type=all&key=' + encodeURIComponent(req.params.key),
-            method: 'GET'
-        };
+        // http://sou.kuwo.cn/ws/NSearch?type=music&key=曾经的你&pn=1
+
+        var key = req.params.key,
+            pn = req.params.pn,
+            option = {
+                host: 'sou.kuwo.cn',
+                port: 80,
+                path: '/ws/NSearch?' + querystring.stringify({
+                    type: 'music',
+                    key: key,
+                    pn: pn
+                }),
+                method: 'GET'
+            };
 
         request(option).then(function(body) {
+
             // 解析 html
             var $ = cheerio.load(body),
                 $list = $('.m_list ul li'),
-                retData = [];
+                $page = $('.page'),
+                list = [];
 
             $list.each(function(i, el) {
                 var $this = $(el);
 
-                retData.push({
+                list.push({
                     id: parseInt($this.find('input[name="musicNum"]').val()),
                     name: $this.find('.m_name a').attr('title'),
                     album: $this.find('.a_name a').attr('title'),
@@ -36,7 +49,26 @@ module.exports = function(app) {
                 });
             });
 
-            res.send(retData);
+            $page.find('a').each(function(i, el) {
+                var $this = $(el),
+                    href = $this.attr('href');
+
+                if (href !== '#@') {
+                    var num = href.split('=').reverse()[0];
+                    $this.attr('data-option', JSON.stringify({
+                        key: key,
+                        pn: num
+                    }));
+                }
+
+                $this.attr('href', 'javascript:;');
+            });
+
+            res.send({
+                page: $page.html(),
+                list: list
+            });
+
         }, function(e) {
             console.log(e);
         });
